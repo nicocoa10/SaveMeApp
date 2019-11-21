5351c58d88b267bb36440f5289ab78ded73edbb1
nicocoa10 <nojeda3@calstatela.edu>
2019-11-20 17:40:33 -0800

    Added navigation components and InformationSaverFragment

--- app/src/main/java/com/example/savemeapp/MainActivity.kt
+++ app/src/main/java/com/example/savemeapp/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.savemeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        val navController = Navigation.findNavController(this,R.id.nav_host_fragment)
+
     }
 }
---
+++ app/src/main/java/com/example/savemeapp/informationsaver/InformationSaverViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.savemeapp.informationsaver
+
+import androidx.lifecycle.ViewModel
+
+class InformationSaverViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
