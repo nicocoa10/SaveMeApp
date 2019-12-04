9561df0da57c5a3c4a28d60db05c89a375139760
nicocoa10 <nojeda3@calstatela.edu>
2019-12-03 19:02:53 -0800

    Did not do correctly last time , so updating everything with new Home fragment , xml file , and drawable files .

--- app/src/main/java/com/example/savemeapp/MainActivity.kt
+++ app/src/main/java/com/example/savemeapp/MainActivity.kt
@@ -2,15 +2,11 @@ package com.example.savemeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-<<<<<<< HEAD
-import androidx.navigation.Navigation
-=======
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.example.savemeapp.databinding.ActivityMainBinding
 
->>>>>>> origin/Danny
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,12 +14,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-<<<<<<< HEAD
-        setContentView(R.layout.activity_main)
-
-//        val navController = Navigation.findNavController(this,R.id.nav_host_fragment)
-
-=======
 
         binding = DataBindingUtil.setContentView(
             this,
@@ -39,7 +29,6 @@ class MainActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         val navController = this.findNavController(R.id.myNavHostFragment)
         return navController.navigateUp()
->>>>>>> origin/Danny
     }
 
 }
